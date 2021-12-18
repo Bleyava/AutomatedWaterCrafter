8ea7f1bcfea7fa45dc2b980c82cf88a04dfc5994
bleyava <bleyava@gmail.com>
2021-12-18 18:21:32 +0100

    init commit. added existing vs project to github repo

---
+++ code/AutoWaterFarmer/AutoWaterFarmer/AutoWaterFarmer/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace AutoWaterFarmer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.lblSpeed = new System.Windows.Forms.Label();
+            this.tmrMouse = new System.Windows.Forms.Timer(this.components);
+            this.lblMouse = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbx1x = new System.Windows.Forms.TextBox();
+            this.tbx1y = new System.Windows.Forms.TextBox();
+            this.tbx2y = new System.Windows.Forms.TextBox();
+            this.tbx2x = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnPossitions = new System.Windows.Forms.Button();
+            this.tbxHbBRY = new System.Windows.Forms.TextBox();
+            this.tbxHbBRX = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxHbTLY = new System.Windows.Forms.TextBox();
+            this.tbxHbTLX = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 91);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(115, 25);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 29);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(115, 50);
+            this.trackBar1.TabIndex = 1;
+            // 
+            // lblSpeed
+            // 
+            this.lblSpeed.AutoSize = true;
+            this.lblSpeed.Location = new System.Drawing.Point(12, 9);
+            this.lblSpeed.Name = "lblSpeed";
+            this.lblSpeed.Size = new System.Drawing.Size(44, 17);
+            this.lblSpeed.TabIndex = 2;
+            this.lblSpeed.Text = "speed";
+            // 
+            // tmrMouse
+            // 
+            this.tmrMouse.Enabled = true;
+            this.tmrMouse.Tick += new System.EventHandler(this.tmrMouse_Tick);
+            // 
+            // lblMouse
+            // 
+            this.lblMouse.AutoSize = true;
+            this.lblMouse.Location = new System.Drawing.Point(12, 189);
+            this.lblMouse.Name = "lblMouse";
+            this.lblMouse.Size = new System.Drawing.Size(43, 17);
+            this.lblMouse.TabIndex = 3;
+            this.lblMouse.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(174, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "crafting in";
+            // 
+            // tbx1x
+            // 
+            this.tbx1x.Location = new System.Drawing.Point(174, 29);
+            this.tbx1x.Name = "tbx1x";
+            this.tbx1x.Size = new System.Drawing.Size(110, 25);
+            this.tbx1x.TabIndex = 5;
+            // 
+            // tbx1y
+            // 
+            this.tbx1y.Location = new System.Drawing.Point(174, 60);
+            this.tbx1y.Name = "tbx1y";
+            this.tbx1y.Size = new System.Drawing.Size(110, 25);
+            this.tbx1y.TabIndex = 6;
+            // 
+            // tbx2y
+            // 
+            this.tbx2y.Location = new System.Drawing.Point(318, 60);
+            this.tbx2y.Name = "tbx2y";
+            this.tbx2y.Size = new System.Drawing.Size(110, 25);
+            this.tbx2y.TabIndex = 9;
+            // 
+            // tbx2x
+            // 
+            this.tbx2x.Location = new System.Drawing.Point(318, 29);
+            this.tbx2x.Name = "tbx2x";
+            this.tbx2x.Size = new System.Drawing.Size(110, 25);
+            this.tbx2x.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(318, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "crafting out";
+            // 
+            // btnPossitions
+            // 
+            this.btnPossitions.Location = new System.Drawing.Point(174, 181);
+            this.btnPossitions.Name = "btnPossitions";
+            this.btnPossitions.Size = new System.Drawing.Size(254, 25);
+            this.btnPossitions.TabIndex = 10;
+            this.btnPossitions.Text = "set possitions";
+            this.btnPossitions.UseVisualStyleBackColor = true;
+            this.btnPossitions.Click += new System.EventHandler(this.btnPossitions_Click);
+            // 
+            // tbxHbBRY
+            // 
+            this.tbxHbBRY.Location = new System.Drawing.Point(318, 150);
+            this.tbxHbBRY.Name = "tbxHbBRY";
+            this.tbxHbBRY.Size = new System.Drawing.Size(110, 25);
+            this.tbxHbBRY.TabIndex = 16;
+            // 
+            // tbxHbBRX
+            // 
+            this.tbxHbBRX.Location = new System.Drawing.Point(318, 119);
+            this.tbxHbBRX.Name = "tbxHbBRX";
+            this.tbxHbBRX.Size = new System.Drawing.Size(110, 25);
+            this.tbxHbBRX.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(318, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "hotbar bottom right";
+            // 
+            // tbxHbTLY
+            // 
+            this.tbxHbTLY.Location = new System.Drawing.Point(174, 150);
+            this.tbxHbTLY.Name = "tbxHbTLY";
+            this.tbxHbTLY.Size = new System.Drawing.Size(110, 25);
+            this.tbxHbTLY.TabIndex = 13;
+            // 
+            // tbxHbTLX
+            // 
+            this.tbxHbTLX.Location = new System.Drawing.Point(174, 119);
+            this.tbxHbTLX.Name = "tbxHbTLX";
+            this.tbxHbTLX.Size = new System.Drawing.Size(110, 25);
+            this.tbxHbTLX.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(174, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "hotbar top left";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 17);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Maus possition";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 245);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbxHbBRY);
+            this.Controls.Add(this.tbxHbBRX);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbxHbTLY);
+            this.Controls.Add(this.tbxHbTLX);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnPossitions);
+            this.Controls.Add(this.tbx2y);
+            this.Controls.Add(this.tbx2x);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbx1y);
+            this.Controls.Add(this.tbx1x);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblMouse);
+            this.Controls.Add(this.lblSpeed);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label lblSpeed;
+        private System.Windows.Forms.Timer tmrMouse;
+        private System.Windows.Forms.Label lblMouse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbx1x;
+        private System.Windows.Forms.TextBox tbx1y;
+        private System.Windows.Forms.TextBox tbx2y;
+        private System.Windows.Forms.TextBox tbx2x;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPossitions;
+        private System.Windows.Forms.TextBox tbxHbBRY;
+        private System.Windows.Forms.TextBox tbxHbBRX;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxHbTLY;
+        private System.Windows.Forms.TextBox tbxHbTLX;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ code/AutoWaterFarmer/AutoWaterFarmer/AutoWaterFarmer/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoWaterFarmer
+{
+    public partial class Form1 : Form
+    {
+        private Point mousePos1 = new Point();
+        private Point mousePos2 = new Point();
+
+        private Point hotbarTopLeft = new Point();
+        private Point hotbarBottomRight = new Point();
+
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+
+        [DllImport("user32.dll",CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+        const uint KEYEVENTF_KEYUP = 0x0002;
+        const uint KEYEVENTF_EXTENDEDKEY = 0x0001;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Thread.Sleep(3000);
+            SendKeys.Send("1");
+        }
+
+        private void tmrMouse_Tick(object sender, EventArgs e)
+        {
+            Point p = new Point();
+
+            GetCursorPos(ref p);
+
+            lblMouse.Text = p.ToString();
+        }
+
+        private void btnPossitions_Click(object sender, EventArgs e)
+        {
+            mousePos1.X = Convert.ToInt32(tbx1x.Text);
+            mousePos1.Y = Convert.ToInt32(tbx1y.Text);
+            mousePos2.X = Convert.ToInt32(tbx2x.Text);
+            mousePos2.Y = Convert.ToInt32(tbx2y.Text);
+
+            hotbarTopLeft.X = Convert.ToInt32(tbxHbTLX.Text);
+            hotbarTopLeft.Y = Convert.ToInt32(tbxHbTLY.Text);
+            hotbarBottomRight.X = Convert.ToInt32(tbxHbBRX.Text);
+            hotbarBottomRight.Y = Convert.ToInt32(tbxHbBRY.Text);
+
+            Thread.Sleep(2000);
+
+            farmWater();
+
+        }
+
+        public void farmWater()
+        {
+            for(int i = 0; i < 2; i++)
+            {
+
+                SendKeys.Send("1");
+
+                for (int x = 0; x < 10; x++)
+                {
+                    leftClick();
+                    Thread.Sleep(25);
+                }
+
+                SendKeys.Send("E");
+                Thread.Sleep(100);
+                craftClearWater();
+                Thread.Sleep(100);
+                prepareHotbar();
+                Thread.Sleep(100);
+                SendKeys.Send("E");
+            }
+        }
+
+        public void prepareHotbar()
+        {
+            int posY = hotbarBottomRight.Y - hotbarTopLeft.Y + hotbarTopLeft.Y;
+
+            int posX = (int) Math.Floor((hotbarBottomRight.X - hotbarTopLeft.X) / 18d) + hotbarTopLeft.X;
+
+            Cursor.Position = new Point(posX, posY);
+
+            shiftKlick();
+            SendKeys.Send("2");
+        }
+
+        public void craftClearWater()
+        {
+            for (int i = 0;i < 10; i++)
+            {
+                Cursor.Position = mousePos1;
+                Thread.Sleep(100);
+                SendKeys.Send(Convert.ToString(i+1));
+                Thread.Sleep(100);
+
+                Cursor.Position = mousePos2;
+                Thread.Sleep(100);
+
+                shiftKlick();
+                Thread.Sleep(100);
+            }
+        }
+
+        public void rightClick()
+        {
+            mouse_event(0x0004, 0, 0, 0, 0);
+            mouse_event(0x0002, 0, 0, 0, 0);
+        }
+
+        public void leftClick()
+        {
+            mouse_event(0x0008, 0, 0, 0, 0);
+            mouse_event(0x0010, 0, 0, 0, 0);
+        }
+
+        public void shiftKlick()
+        {
+            pressShift();
+            Thread.Sleep(100);
+            rightClick();
+            Thread.Sleep(100);
+            releaseShift();
+        }
+
+        public void pressShift()
+        {
+            keybd_event((byte)0x10, 0x2A, 1, 0);
+        }
+
+        public void releaseShift()
+        {
+            keybd_event((byte)0x10, 0x2A, 3, 0);
+        }
+    }
+}
